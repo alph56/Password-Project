954d301e7128ba3e3b9ce7c65ebbc2d7414b9112
Alejandro Chavez <118617128+Elwoodyhub@users.noreply.github.com>
2022-11-26 16:35:02 +0000

    Creacion basica del menu

---
+++ .ccls-cache/@home@runner@Password-Project/menu.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int opcion;
+
+int main(){
+  
+cout<<"----Bienvenido----"<<endl;
+cout<<"Que desea hace"<<endl;  
+
+switch (opcion)  {
+    case 1:
+        cout<<"Crear nuevo usuario y contraseña de un nuevo sitio"<<endl;
+        break;
+
+    case 2:
+        cout<<"Ver usuarios y contraseñas de todos los sitios"<<endl;
+        break;
+    case 3:
+        cout<<"Ver usuario y contraseña de un sitio en especifico "<<endl;
+        break;
+        
+        
+    default:
+        // code to be executed if
+        // expression doesn't match any constant
+}
+
+ 
+
+ 
+  }
