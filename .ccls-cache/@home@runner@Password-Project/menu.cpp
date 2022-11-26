45cc5cde47d7f284e6b8bec27b762913d4c0feb3
Alejandro Chavez <118617128+Elwoodyhub@users.noreply.github.com>
2022-11-26 18:01:29 +0000

    Agregado eliminar y modificar contraseña

--- .ccls-cache/@home@runner@Password-Project/menu.cpp
+++ .ccls-cache/@home@runner@Password-Project/menu.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
-#include lista.h
+//#include lista.h
 using namespace std;
 
-int opcion;
-int c;
+
+
 int main(){
+  int opcion;
+int c;
+int sit;
+int contra;
+int usu;
   
 cout<<"----Bienvenido----"<<endl;
 cout<<"Que desea hacer"<<endl;  
 cout<<"Crear usuario y contraseña de un nuevo sitio"<<endl;
-cout<<"Ver usuarios y contraseñas de todos los sitios"<<endl;
-cout<<"Ver usuario y contraseña de un sitio en especifico "<<endl;
+cout<<"Ver todos los sitios"<<endl;
+cout<<"Ver usuario y contraseña de un sitio en especifico "
+<<endl;
+cout<<"Eliminar contraseña"<<endl;
+cout<<"Modificar contraseñas"<<endl;  
 switch (opcion)  {
     case 1:
 
       cout<<"sitio"<<endl;
-        cin<<sit;
+        cin>>sit;
       
       cout<<"usuario"<<endl;
-        cin<<usiario;
+        cin>>usu;
       
       cout<<"crear contraseña nueva"<<endl;
       cout<<"generar contraseña nueva"<<endl;
@@ -37,14 +45,15 @@ switch (opcion)  {
         break;
 
     case 2:
-        cout<<"Contraseña:  ";
-      cin>>contraseñagen;
+        cout<<"Estos son los sitios"<<endl;
       
         break;
     case 3:
-        
+        cout<<"Estos son los usiarios y contraseñas";
         break;
-        
+  case 4:
+
+  break;
         
     default:
   cout<<"Error, Esa no es una opcion valida"<<endl;
