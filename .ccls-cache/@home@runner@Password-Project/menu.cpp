35062b3666c9af2e16d777d97fcddd443de68b7f
Alejandro Chavez <118617128+Elwoodyhub@users.noreply.github.com>
2022-11-26 17:14:40 +0000

    Actualizacion del menu

--- .ccls-cache/@home@runner@Password-Project/menu.cpp
+++ .ccls-cache/@home@runner@Password-Project/menu.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
-
+#include lista.h
 using namespace std;
 
 int opcion;
-
+int c;
 int main(){
   
 cout<<"----Bienvenido----"<<endl;
-cout<<"Que desea hace"<<endl;  
-
+cout<<"Que desea hacer"<<endl;  
+cout<<"Crear usuario y contraseña de un nuevo sitio"<<endl;
+cout<<"Ver usuarios y contraseñas de todos los sitios"<<endl;
+cout<<"Ver usuario y contraseña de un sitio en especifico "<<endl;
 switch (opcion)  {
     case 1:
-        cout<<"Crear nuevo usuario y contraseña de un nuevo sitio"<<endl;
+
+      cout<<"sitio"<<endl;
+        cin<<sit;
+      
+      cout<<"usuario"<<endl;
+        cin<<usiario;
+      
+      cout<<"crear contraseña nueva"<<endl;
+      cout<<"generar contraseña nueva"<<endl;
+      switch(c)
+      {
+        case 1:
+      cout<<"contraseña"<<endl;
+        cin>>contra;
+        break;
+        case 2:
+        cout<<"generando contraseña"<<endl;
+        break;
+        default:
+        cout<<"Error,Esa no es una opcion valida"<<endl;
+      }
         break;
 
     case 2:
-        cout<<"Ver usuarios y contraseñas de todos los sitios"<<endl;
+        cout<<"Contraseña:  ";
+      cin>>contraseñagen;
+      
         break;
     case 3:
-        cout<<"Ver usuario y contraseña de un sitio en especifico "<<endl;
+        
         break;
         
         
     default:
+  cout<<"Error, Esa no es una opcion valida"<<endl;
+  break;
         // code to be executed if
         // expression doesn't match any constant
 }
