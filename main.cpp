e100ebe5be490e06cbc23d7e1840b460abafab32
alph56 <111250517+alph56@users.noreply.github.com>
2022-12-01 23:58:31 +0000

    main blank for the merge

--- main.cpp
+++ main.cpp
@@ -3,28 +3,5 @@
 using namespace std;
 
 int main() {
-  Lista*list=new Lista();
-  Credencial c1;
-  c1.dominio="www.google.com";
-  string a="alph56";
-  string b="Hola987";
-  c1.Setpassword(b);
-  c1.SetUsuario(a);
-  list->AppendL(c1);
-
-  c1.dominio="www.epicgames.com";
-  string c="newplayer01";
-  string d="contrasena132";
-  c1.Setpassword(d);
-  c1.SetUsuario(c);
-  list->AppendL(c1);
-  list->MostrarTodoP();
-  list->Tofile();
-  string f="www.google.com";
-  list->BuscarList(f);
-  list->Eliminar(f);
-  list->MostrarTodoP();
-
-  //list->EliminarTodo();
-  //list->MostrarTodoP();
+ 
 }
