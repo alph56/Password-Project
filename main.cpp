2a434db1623e12577f93a78da33c2721c12e2c5a
Elwoodyhub <118617128+Elwoodyhub@users.noreply.github.com>
2022-12-01 20:04:06 -0600

    Update main.cpp
    Mejora del menú, inicio de ajustes para funciones de la lista y el generador de contraseñas

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include<locale.h>
 #include"Lista.h"
-#include"GenPass.h"
+//#include"GenPass.h"
 using namespace std;
 
 int main() {
   //Idioma:Español
   setlocale(LC_CTYPE, "Spanish");
-  int opcion;
+  //OBJETOS
+  //GenPass*mainpass=new GenPass();
+  Lista*list=new Lista();
+  string contra,comparar;
+  //OPCIONES DE SWITCH
+  int opcion,anws;
+  
+  
   cout<<"----Bienvenido----"<<endl;
   do{
   cout<<"¿Qué desea hacer?"<<endl;
@@ -18,8 +25,43 @@ int main() {
   cin>>opcion;
   switch(opcion){
     case 1:
+      cout<<"¿Qué tipo de contraseña quiere realizar?"<<endl;
+      cout<<"1.-Generar una manualmente"endl;
+      cout<<"2.-Generar una aleatoriamente"<<endl;
+      cout<<"3.-Regresar"<<endl;
+      cout<<"Inserte una opcion: ";
+      cin>>anws;
+      switch(answ){
+        case 1:
+          system("cls");
+          //mainpass->Manual(contra);
+          system("pause");
+          break;
+        case 2:
+          system("cls");
+          //mainpass->RandomPass(contra);
+          system("pause");
+          break;
+        case 3:
+          system("cls");
+          cout<<"--->Saliendo...."<<endl;
+          break;
+        default:
+          system("cls");
+          cout<<"Opción NO válida"<<endl;
+          system("pause");
+          break;
+      }
       break;
     case 2:
+      cout<<"Ingrese su contraseña:"<<endl;
+      cout<<"---->: ";
+      cin>>comparar;
+      if(comparar){
+      }
+      else{
+        cout<<"contraseña Incorrecta"<<endl;
+      }
       break;
     case 3:
       system("cls");
