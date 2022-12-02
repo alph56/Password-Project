ce11385cf36cc59d33e5e22f6c639a4588e9765b
Elwoodyhub <118617128+Elwoodyhub@users.noreply.github.com>
2022-12-01 22:25:28 -0600

    Update main.cpp
    Avance del menú: opciones habilitadas, de momento, hasta la opción 3 del segundo menú

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main() {
   //OBJETOS
   //GenPass*mainpass=new GenPass();
   Lista*list=new Lista();
-  string contra,comparar;
+  Credencial c1;
+  string contra,comparar,passc,usuarc;
   //OPCIONES DE SWITCH
   int opcion,anws,selection;
   
@@ -60,15 +61,99 @@ int main() {
       if(){
         cout<<"Bienvenido a el menú gestor"<<endl;
         do{
-          cout<<"1.-Ver Sitios Agregados"<<endl;
-          cout<<"2.-Ver Sitios con contraseña y usuario"<<endl;
-          cout<<"6.-Salir del menú gestor"<<endl;
-        }while(selection!=6);
-        
+          cout<<"1.-Agregar Sitio nuevo"<<endl;
+          cout<<"2.-Ver Todos los sitios Agregados"<<endl;
+          cout<<"3.-Ver Todos los sitios con contraseña y usuario"<<endl;
+          cout<<"4.-Buscar nombre de Sitio"<<endl;
+          cout<<"5.-Modificar contraseña de Sitio"<<end1;
+          cout<<"6.-Modificar usuario de Sitio"<<end1;
+          cout<<"7.-Modificar nombre de Sitio"<<endl;
+          cout<<"8.-Eliminar Sitio"<<endl;
+          cout<<"9.-Eliminar Todo"<<endl;
+          cout<<"10.-Salir del menú gestor"<<endl;
+          cout<<"Inserte una opcion: ";
+          cin>>selection;
+          switch(seletion){
+            case 1:
+              system("cls");
+              cout<<"Inserte el nombre del sitio: ";
+              cin>>c1.dominio;
+              cout<<"Inserte su nombre de usuario de ese sitio web: ";
+              cin>>usuarc;
+              c1.SetUsuario(usuarc);
+              cout<<"Inserte su contraseña de ese sitio web: ";
+              cin>>passc;
+              c1.Setpassword(passc);
+              list->AppendL(c1);
+              system("pause");
+              break;
+            case 2:
+              cout<<"Por cuestiones de Privacidad Usuario y Contraseña se mantienen ocultos"<<endl;
+              list->MostrarTodoP();
+              break;
+            case 3:
+              system("cls");
+              cout<<"Ingrese su contraseña:"<<endl;
+              cout<<"---->: ";
+              cin>>comparar;
+              if(){
+                system("cls");
+                list->MostrarTodo();
+                system("pause");
+              }
+              else{
+                system("cls");
+                cout<<"contraseña Incorrecta"<<endl;
+                system("pause");
+              }
+              break;
+            case 4:
+              system("cls");
+              
+              system("pause");
+              break;
+            case 5:
+              system("cls");
+              
+              system("pause");
+              break;
+            case 6:
+              system("cls");
+              
+              system("pause");
+              break;
+            case 7:
+              system("cls");
+              
+              system("pause");
+              break;
+            case 8:
+              system("cls");
+              
+              system("pause");
+              break;
+            case 9:
+              system("cls");
+              
+              system("pause");
+              break;
+            case 10:
+              system("cls");
+              cout<<"--->Regresando al menú de inicio...."<<endl;
+              break;
+            default:
+              system("cls");
+              cout<<"Opción NO válida"<<endl;
+              system("pause");
+              break;
+          }
+        }while(selection!=10);
       }
       else{
+        system("cls");
         cout<<"contraseña Incorrecta"<<endl;
-      }*/
+        system("pause");
+      }
       break;
     case 3:
       system("cls");
