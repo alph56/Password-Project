79c3d86857d8212aad22a635244fb1a47d3da531
Elwoodyhub <118617128+Elwoodyhub@users.noreply.github.com>
2022-12-03 13:55:12 -0600

    Create GenPass.h

---
+++ GenPass.h
@@ -0,0 +1,23 @@
+#ifndef PASSWORD_H
+#define PASSWORD_H
+#include<iostream>
+
+using namespace std;
+
+class GenPass {
+private:
+	string contrasenhia;
+public:
+
+	GenPass();
+	void setcontrasenhia(string);
+	string getcontrasenhia(void);
+
+	void RandomPass(int);
+  void ManualPass(string);
+  int Verificador(string);
+	void escribir(void);
+  void leer(void);
+};
+
+#endif
